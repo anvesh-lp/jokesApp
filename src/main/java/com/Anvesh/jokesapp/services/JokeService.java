8d9eb8655c7fa17394b43658fca2c05c0147926c
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-23 11:23:08 +0530

    Initital test commit branch

---
+++ src/main/java/com/Anvesh/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.Anvesh.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/Anvesh/jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.Anvesh.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Override
+    public String getJoke() {
+//        Testing branch
+
+        return null;
+    }
+}
